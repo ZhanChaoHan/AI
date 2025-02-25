9898fc79761276fbce544cc603e4dfbe594c11ca
jachs <799516423@qq.com>
2025-02-25 11:17:48 +0800

    'ees'

--- tongyi/src/test/java/com/jachs/tongyi/cosplay/Demo.java
+++ tongyi/src/test/java/com/jachs/tongyi/cosplay/Demo.java
@@ -1,4 +1,4 @@
-package com.jachs.tongyi.word_art;
+package com.jachs.tongyi.cosplay;
 
 import java.io.IOException;
 
@@ -10,50 +10,29 @@ import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 
-/****
- * @see https://bailian.console.aliyun.com/?spm=5176.29597918.J__Xz0dtrgG-8e2H7vxPlPy.7.1e8c7ca0IZzWV7#/model-market/detail/wordart-surnames?tabKey=sdk
- * @author hanchaohan
+/***
+ * 本文介绍通义万相-Cosplay动漫人物模型的输入输出参数。通过输入人像图片和卡通形象图片，可快速生成人物卡通写真。
+ * @see https://help.aliyun.com/zh/model-studio/user-guide/cosplay-anime-character-generation?spm=0.0.0.i1
+ * @author zhanchaohan
  */
 public class Demo {
     OkHttpClient client = new OkHttpClient();
-    public static String URL="https://dashscope.aliyuncs.com/api/v1/services/aigc/wordart/surnames";
+    public static String URL="https://dashscope.aliyuncs.com/api/v1/services/aigc/image-generation/generation";
     public static final MediaType JSON= MediaType.get("application/json; charset=utf-8");
     
     @Test
     public void t1() throws Exception {
         String json="""
-           {
-                "model": "wordart-surnames",
-                "prompt": "白云，蓝天", 
-                "input":{
-                    "surname": "诜羽",
-                    "style": "peerless_beauty"
-                },
-                "parameters": {
-                    "n": 1
+             {
+                "model": "wanx-style-cosplay-v1",
+                "input": {
+                    "model_index": 1,
+                    "face_image_url": "https://www.pnglog.com/xx9PWT.jpg",
+                    "template_image_url": "https://www.pnglog.com/qh2lx7.jpg"
                 }
             }
              """;
         
-        //自定义
-        /* String json="""
-               {
-                    "text": {
-                        "font_name": "gufeng1",
-                        "ttf_url": null,
-                        "text_strength": 0.5,
-                        "text_inverse": false
-                    },
-                    "surname": "赵",
-                    "style": "diy",
-                    "prompt": "高山，流水", 
-                    "ref_image_url": null
-                },
-                "parameters": {
-                    "n": 1
-                }
-            }
-                  """;*/
         RequestBody body = RequestBody.create(json, JSON);
         
         Request request = new Request.Builder()
@@ -68,12 +47,11 @@ public class Demo {
         System.out.println ( response.body().string());
     }
     
-    
-   //取去task_id获取图片
+    //取去task_id获取视频
     @Test
     public void getMp4() throws IOException {
         String url="https://dashscope.aliyuncs.com/api/v1/tasks/";
-        String task_id="e7154218-b856-4867-a891-0701763af20d";
+        String task_id="c7f433fb-c7f8-4afe-ac50-473f0ca189d7";
         Request request = new Request.Builder()
                 .addHeader ( "Authorization", "Bearer "+System.getenv("DASHSCOPE_API_KEY") )//Bearer 必须带后面是自己的key
                 .url(url+task_id)
